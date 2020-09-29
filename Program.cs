cd332ff1c08e6b6e304fd304883b4653612d8f24
ABC <abc@gmail.com>
2020-09-30 02:16:50 +0530

    UC 4

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Start
             }
 
         };
-        public static string distance(Point p1, Point q1, Point p2, Point q2)
+        public static void distance(Point p1, Point q1, Point p2, Point q2)
         {
             // for details of below formula. 
             /*int val = (q.y - p.y) * (r.x - q.x) -
@@ -46,14 +46,14 @@ namespace Start
         }
         static void Main(string[] args)
         {
-            int x1 = Console.ReadLine();
-            int x2 = Console.ReadLine();
-            int y1 = Console.ReadLine();
-            int y2 = Console.ReadLine();
-            int x12 = Console.ReadLine();
-            int y12 = Console.ReadLine();
-            int y21 = Console.ReadLine();
-            int x21 = Console.ReadLine();
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            int x12 = Convert.ToInt32(Console.ReadLine());
+            int y12 = Convert.ToInt32(Console.ReadLine());
+            int y21 = Convert.ToInt32(Console.ReadLine());
+            int x21 = Convert.ToInt32(Console.ReadLine());
             Point p1 = new Point(x1, y1);
             Point q1 = new Point(x2, y2);
             Point p2 = new Point(x12, y12);
