09b904a7918c6b80a4e6eabc9a904149d36c3b27
ABC <abc@gmail.com>
2020-09-29 18:39:49 +0530

    UC 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Start
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line Comparison Computation Program on Master Branch");
+            int x1 = Console.ReadLine();
+            int x2 = Console.ReadLine();
+            int y1 = Console.ReadLine();
+            int y2 = Console.ReadLine();
+            double d = getdistance(x1, x2, y1, y2);
+        }
+        static double getdistance(int x1, int x2, int y1, int y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
