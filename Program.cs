dcb8047111dc0eecbc9e8863157bd2df376fcb0b
ABC <abc@gmail.com>
2020-09-29 18:29:55 +0530

    start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line Comparison Computation Program on Master Branch");
+        }
+    }
+}
